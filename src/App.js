2a3c9d9bea338aed97aa86cf49f8ecd0306029bb
v8phan <56612604+v8phan@users.noreply.github.com>
2021-07-31 17:04:10 -0500

    created forms and attempting to call api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      statestate: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange (evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  handleSubmit(event) {
+    alert('You are in ' + this.state.city +', ' + this.state.statestate);
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=02d2c0bc0569091f6d80053853dfabd6`)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          window.alert("server not connected?")
+          console.log(error)
+        })
+
+   // event.preventDefault();
+  }
+  componentDidUpdate() {
+    console.log(this.state.city +', ' + this.state.statestate)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+      <h1>Hello </h1>
+      <p>Enter your city:</p>
+      <input
+        type='text'
+        name='city'
+        value={this.state.city}
+        onChange={this.handleChange}
+      />
+      <p>Enter your state:</p>
+      <input
+        type='text'
+        name='statestate'
+        value={this.state.statestate}
+        onChange={this.handleChange}
+      />
+      <input type='submit' value="Can I hit?"/>
+
+      </form>
+    );
+
+  }
+
 }
+
 
 export default App;
