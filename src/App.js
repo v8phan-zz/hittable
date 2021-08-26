a49d68f51cc64689e5140b77f5a247ea148ec14c
v8phan <56612604+v8phan@users.noreply.github.com>
2021-08-25 23:42:04 -0500

    styling stuff

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 const styles = {
+  bigDiv: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    marginTop: 0,
+    backgroundColor: "orange",
+  },
   form: {
     paddingLeft: 20,
   },
@@ -34,7 +43,6 @@ class App extends React.Component {
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    //this.setState = this.setState.bind(this);
   }
 
   handleChange(evt) {
@@ -44,42 +52,40 @@ class App extends React.Component {
   componentDidMount() {
     if ("geolocation" in navigator) {
       console.log("Available");
-      navigator.geolocation.getCurrentPosition(function(position) {
-        console.log("Latitude is :", position.coords.latitude);
-        console.log("Longitude is :", position.coords.longitude);
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          console.log("Latitude is :", position.coords.latitude);
+          console.log("Longitude is :", position.coords.longitude);
 
-        axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=02d2c0bc0569091f6d80053853dfabd6`
-        )
-        .then((response) => {
-        
-          console.log(response);
-          if (response.data.weather[0].description === "heavy rain") {
-            this.setState({stateDisplayMessage: "Too rainy!"});
-          } else if (response.data.main.temp < 60) {
-            this.setState({stateDisplayMessage: "Too cold!"});
-          } else if (response.data.wind.speed > 10) {
-            this.setState({stateDisplayMessage: "Too windy!"});
-          } else {
-            this.setState({stateDisplayMessage: `It's ${response.data.main.temp} degrees with average winds of ${response.data.wind.speed} mph. Hittable!`});
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-      }.bind(this));
-  
-      
-
+          axios
+            .get(
+              `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=02d2c0bc0569091f6d80053853dfabd6`
+            )
+            .then((response) => {
+              console.log(response);
+              if (response.data.weather[0].description === "heavy rain") {
+                this.setState({ stateDisplayMessage: "Too rainy!" });
+              } else if (response.data.main.temp < 55) {
+                this.setState({ stateDisplayMessage: "Too cold!" });
+              } else if (response.data.wind.speed > 10) {
+                this.setState({ stateDisplayMessage: "Too windy!" });
+              } else {
+                this.setState({
+                  stateDisplayMessage: `It's ${response.data.main.temp} degrees with average winds of ${response.data.wind.speed} mph. Hittable!`,
+                });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }.bind(this)
+      );
     } else {
       console.log("Not Available");
     }
   }
 
-
   handleSubmit(event) {
-
     alert("You are in " + this.state.city + ", " + this.state.stateinput);
 
     const statecodeObject = statecodes.find(
@@ -87,20 +93,21 @@ class App extends React.Component {
     );
     console.log(statecodeObject.Code);
 
-    
     axios
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${this.state.city},${statecodeObject.Code}&units=imperial&appid=02d2c0bc0569091f6d80053853dfabd6`
       )
       .then((response) => {
         if (response.data.weather[0].description === "heavy rain") {
-          this.setState({stateDisplayMessage: "Too rainy!"});
+          this.setState({ stateDisplayMessage: "Too rainy!" });
         } else if (response.data.main.temp < 60) {
-          this.setState({stateDisplayMessage: "Too cold!"});
+          this.setState({ stateDisplayMessage: "Too cold!" });
         } else if (response.data.wind.speed > 10) {
-          this.setState({stateDisplayMessage: "Too windy!"});
+          this.setState({ stateDisplayMessage: "Too windy!" });
         } else {
-          this.setState({stateDisplayMessage: `It's ${response.data.main.temp} degrees with average winds of ${response.data.wind.speed} mph. Hittable!`});
+          this.setState({
+            stateDisplayMessage: `It's ${response.data.main.temp} degrees with average winds of ${response.data.wind.speed} mph. Hittable!`,
+          });
         }
         console.log(response.data);
       })
@@ -119,39 +126,41 @@ class App extends React.Component {
     const { classes } = this.props;
 
     return (
-      <div>
-      <form onSubmit={this.handleSubmit} className={classes.form}>
-        <h1>Can I play tennis today? </h1>
-        <p>Enter your city:</p>
-        <TextField
-          variant="outlined"
-          type="text"
-          name="city"
-          value={this.state.city}
-          label="City"
-          onChange={this.handleChange}
-        />
-        <p>Enter your state:</p>
-        <TextField
-          variant="outlined"
-          type="text"
-          name="stateinput"
-          value={this.state.stateinput}
-          label="State"
-          id="outlined-basic"
-          onChange={this.handleChange}
-        />
-        <br></br>
-        <Button
-          size="large"
-          className={classes.button}
-          type="submit"
-          variant="outlined"
-        >
-          Can I hit?
-        </Button>
-      </form>
-      <p className={classes.displaymessage}>{this.state.stateDisplayMessage}</p>
+      <div className={classes.bigDiv}>
+        <form onSubmit={this.handleSubmit} className={classes.form}>
+          <h1>Can I play tennis today?</h1>
+          <p>Enter your city:</p>
+          <TextField
+            variant="outlined"
+            type="text"
+            name="city"
+            value={this.state.city}
+            label="City"
+            onChange={this.handleChange}
+          />
+          <p>Enter your state:</p>
+          <TextField
+            variant="outlined"
+            type="text"
+            name="stateinput"
+            value={this.state.stateinput}
+            label="State"
+            id="outlined-basic"
+            onChange={this.handleChange}
+          />
+          <br></br>
+          <Button
+            size="large"
+            className={classes.button}
+            type="submit"
+            variant="outlined"
+          >
+            Can I hit?
+          </Button>
+        </form>
+        <p className={classes.displaymessage}>
+          {this.state.stateDisplayMessage}
+        </p>
       </div>
     );
   }
